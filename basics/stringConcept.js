79b7745b8c4781370b0f2a535126a2c159d10846
ManiKrishna <mani@hackerearth.com>
2025-07-10 10:41:19 +0530

    Add JavaScript and TypeScript examples for string manipulation

---
+++ basics/stringConcept.js
@@ -0,0 +1,8 @@
+var str = "Hello, World!";
+console.log(str);
+console.log("String length: ".concat(str.length));
+console.log("First character: ".concat(str.charAt(0)));
+console.log("Substring (0, 5): ".concat(str.substring(0, 5)));
+console.log("Uppercase: ".concat(str.toUpperCase()));
+console.log("Lowercase: ".concat(str.toLowerCase()));
+console.log("Contains 'World': ".concat(str.includes("World")));
---
+++ basics/stringConcept.ts
@@ -0,0 +1,9 @@
+let str:string = "Hello, World!";
+console.log(str);
+
+console.log(`String length: ${str.length}`);
+console.log(`First character: ${str.charAt(0)}`);
+console.log(`Substring (0, 5): ${str.substring(0, 5)}`);
+console.log(`Uppercase: ${str.toUpperCase()}`);
+console.log(`Lowercase: ${str.toLowerCase()}`);
+console.log(`Contains 'World': ${str.includes("World")}`);
